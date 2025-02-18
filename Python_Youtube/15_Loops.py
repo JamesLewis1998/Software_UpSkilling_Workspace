ea7394e65ef27621aaa4d181a3e05e5f3add18cd
James Dickinson <jdickin2@jaguarlandrover.com>
2025-02-18 14:19:00 +0000

    While Loop Example Complete

--- Python_Youtube/15_Loops.py
+++ Python_Youtube/15_Loops.py
@@ -1,11 +1,41 @@
-# ========== TITLE ==========
+# ========== LOOPS ==========
 
-print( "Insert Section Title here")
+print( "Loops")
 
-## ======= SUBTITLE1 =========
+## ======= Loops Overview =========
+    # Essential Part of programming, in pyhton we have 
+        # While Loops 
+        # For Loops
 
+# While Loop Example
 
+# condition = True                    # Condition defined as True
+# while condition == True:            # While Condition is True the following is evaluated
+#     print ("The Condition is True") # When this is executed the body of the function will run indefinitely in the command window
+                                    # Do not run this as the function will infinitely print this into the command window
 
+# To Fix this from happening on the command line, define the following:
+
+condition1 = True
+while condition1 == True:           # Whilst Condition1 is True the following statement is printed
+    print("The condition1 is True")
+    condition1 = False              # Now the Loop runs once - in this case first iteration is run as condition1 is evaluated as true
+                                    # Then following this in the body of the Loop the condition1 is then evaluated to False and the condition1 is 
+                                    # Then evaluated as False and control goes to the next instruction in the loop - for which there isn't one in this case
+
+# It's common to add an iteration to stop the Loop after some number of cycles
+    # For Example here's a While Loop with a Counter: 
+
+count = 0                           # Count starts at 0 
+while count < 10:                   # Whilst count is below 10, execute the following
+    print("The condition is True")  # Print in the command window
+    count = count + 1               # Increments the counter every time until we get to the end
+                                    # count outside is initialisation value almost -> count begins at 0
+                                    # count internal to while function then runs through 1-10 internal to function
+                                    # Until count == 10 then the while Loop is exited and we move to:
+print ("After the Loop")            # After count == 10 then code moves to this print condition and prints "After Loop in Cmd window"
+
+# For Loop Example:
 
 
 ## ======= SUBTITLE2 =========
