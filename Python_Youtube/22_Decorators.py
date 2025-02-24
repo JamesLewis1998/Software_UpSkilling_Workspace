313880efc2ccaef2e3433ce0631db00b4c76a913
James Dickinson <jdickin2@jaguarlandrover.com>
2025-02-24 18:20:02 +0000

    Completed Decorators Section, moving onto DocStrings as per timestamp: https://youtu.be/eWRfhZUzrAc?t=11207

--- Python_Youtube/22_Decorators.py
+++ Python_Youtube/22_Decorators.py
@@ -11,11 +11,15 @@ def Hello():
     print("Hello!")
 
 # To add a decorator to the above do the following: 
-@logtime                                            # Function now has the log time decorator assigned
+# @logtime                                            # Function now has the log time decorator assigned
 def hello():                                        # Therefore whenever we call the hello function, the decorator is going to be called
     print("Hello!")
+    # The above on it's own won't return anything -> you'll get an error in the command window as follows:
+        # Because log time as a function needs to be defined
+        # Error in Terminal: NameError: name 'logtime' is not defined
 
-# A decorator is a function that takes a function as a parameter, wraps the function in an inner function that performs the job it has to do and returns that inner function
+# A decorator is a function that takes a function as a parameter, 
+# wraps the function in an inner function that performs the job it has to do and returns that inner function
 
 # For Example, another function defined as log time where we'll do something before and after the function
     # You want to use decorators to change the behaviour of a function without modifying the function itself
@@ -24,22 +28,30 @@ def hello():                                        # Therefore whenever we call
         # perform caching
         # verify permissions etc etc etc
 
-def logtime(func):
-    def wrapper():
-        print("before")
+def logtime(func):                                  # Creat function called logtime function
+    def wrapper():                                  
+        print("before")                             # Print "Before" before the function runs
         val = func()
-        print("after")
-        return val 
+        print("after")                              # Print "After" after the function runs
     return wrapper
+    # Return wrapper returns the before, the function hello() and the after function
 
-## ======= SUBTITLE2 =========
+@logtime                                            # Logtime defined above 
+def hello():                                        # As before hello() function defined
+    print("Hello!")
 
-
-
-
-
-## ======= SUBTITLE3 =========
-
-
+hello()                                             # Now we call the function 
+                                                    # The command window returns 
+                                                        # before
+                                                        # Hello!
+                                                        # after 
+    # Running hello() allows you to run logtime above in conjunction allowing us to define the before and after commands to run
+        # hello() function wrapped into inner function being "wrapper" which returns the inner function
+        # logtime(func) returns wrapper which prints:
+            # before
+            # funct (which == Hello!) 
+            # after 
+        # into the command line
+        # As you can see above the function wrapper returns the threee above with the return of the logtime(func) defined as wrapper
 
 
