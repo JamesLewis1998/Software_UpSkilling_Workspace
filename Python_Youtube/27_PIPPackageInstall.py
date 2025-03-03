0bc175900a5e36b2cd311f71f70afe37f6de6418
James Dickinson <jdickin2@jaguarlandrover.com>
2025-03-03 19:45:25 +0000

    Pip Course overview completion and List Compression Demonstrate vs using Loop or lamdba function with list/map

--- Python_Youtube/27_PIPPackageInstall.py
+++ Python_Youtube/27_PIPPackageInstall.py
@@ -1,19 +1,27 @@
-# ========== TITLE ==========
+# ========== Third Party Packages and PIP ==========
 
-print( "Insert Section Title here")
+print( "Installing Third Party Packages Using PIP")
 
-## ======= SUBTITLE1 =========
+## ======= Installing Third Party Packages in Python Using PIP =========
+    # Python standard library contains large number of utlilies which simply our python development needs
+        # However, not one utility alone can of course satisy all a users needs
+        # This is why individuals and companies create packages and make them available as open source sw for the entire community 
+    # Modules are all collected in a single place called Python Package Index
+        # Available at pypi.org
+        # Now these packages can be installed on the system using PIP
+        # Most computers already have PIP installed
+        # Over 270,000 packages freely available
 
-
-
-
-
-## ======= SUBTITLE2 =========
-
-
-
-
-
-## ======= SUBTITLE3 =========
-
-
+# How to install a package:
+    # Open up command line and type in pip install
+        # pip install requests --> installs requests package and makes package available for all python scripts
+        # Can also upgrade package to it's latest version by putting:
+            # pip install - u requests
+            # In this case here in command line would upgrade from 2.2.0 to 2.2.1
+        # Note, can also classify specific version when you're installing
+    # You can also uninstall a package by running
+        # pip uninstall requests
+    # If you want to find some information out about the package you can also run 
+        # pip show requests 
+            # where show is used to request more detail about the requests
+                # Including the name, the version the summary etc.
--- Python_Youtube/28_ListCompression.py
+++ Python_Youtube/28_ListCompression.py
@@ -1,19 +1,34 @@
-# ========== TITLE ==========
+# ========== List Compression ==========
 
-print( "Insert Section Title here")
+print( "List Compression Summary")
 
-## ======= SUBTITLE1 =========
+## ======= List Compression Overview =========
+    # List Compressions are a way to create lists in a very concise way 
 
+# For Example, create a list of numbers: 
+numbers = [1,2,3,4,5]
 
+# Now create a new list using list compressions composed by the numbers list elements to the power of 2:
+numbers_power_2 = [n**2 for n in numbers]
+print(numbers_power_2)          # Produces a list in command window of every number to the power of 2
+                                # Note the syntax above is whats used to create list compressions
+                                    # ie n**2 is the command to put the entry to the power of 2
+                                    # for n in numbers is the way to generate the list
 
+# Note, list compressions are a syntax that's sometimes preferred over loops as its more readable and the operation can be written on a single line
 
+# This is how you do it in a loop
+    # What you do in a single line above takes multiple lines to do the following: 
 
-## ======= SUBTITLE2 =========
+numbers_power_2_loop = []       # Define an empty list
+for n in numbers:               # Define for loop entry condition -> for n 
+    numbers_power_2_loop.append(n**2)
+print(numbers_power_2_loop)
 
+# This is how you would do it using map:
 
+numbers_power_2_map = list(map(lambda n : n**2,numbers))
+print(numbers_power_2_map)
 
-
-
-## ======= SUBTITLE3 =========
-
-
+# Again, both the loop and list approach above are more complex
+    # It's simpler in both cases here to use the list compression syntax and makes for more efficient code/ programming
