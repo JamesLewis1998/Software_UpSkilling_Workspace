44cb3253e7efae99f686e52f060a254224e40975
James Dickinson <jdickin2@jaguarlandrover.com>
2025-03-14 11:43:32 +0000

    Missing parenthesis for play at the Game

--- Python_Youtube/BlackJack_Programme/V1_BlackJack_Baseline_Programme_Release.py
+++ Python_Youtube/BlackJack_Programme/V1_BlackJack_Baseline_Programme_Release.py
@@ -1,4 +1,4 @@
-import random   # Import Random Python Module to access random.shuffle function
+import random
 
 class Card:
     def __init__(self,suit,rank): 
@@ -9,9 +9,9 @@ class Card:
 
 class Deck: 
     def __init__(self):
-        self.cards = []                                     # New variable cards created with empty list assigned
-        suits = ["Spades","Clubs","Hearts","Diamonds"]      # List of suits
-        ranks = [                                           # List of ranks -> key value pairs
+        self.cards = []
+        suits = ["Spades","Clubs","Hearts","Diamonds"]
+        ranks = [
                 {"rank": "ACE" , "value": 11},
                 {"rank": "2" , "value": 2},
                 {"rank": "3" , "value": 3},
@@ -28,21 +28,19 @@ class Deck:
             ]
         for suit in suits:
             for rank in ranks:
-                self.cards.append(Card(suit, rank))          # Append Suit and Rank to empty Card List above 
-
+                self.cards.append(Card(suit, rank))
 
     def shuffle(self):
         if len(self.cards) > 1:
-            random.shuffle(self.cards)                      # Now shuffle the cards using the random function
+            random.shuffle(self.cards)
 
-    def deal(self, number):                                  # number as input to the number of cards to be dealt
+    def deal(self, number):
         cards_dealt = []
         for x in range(number):
             if len(self.cards) > 0:
                 card = self.cards.pop()
                 cards_dealt.append(card)
         return cards_dealt
-
 
 class Hand():
     def __init__(self, dealer=False):
@@ -75,6 +73,7 @@ class Hand():
 
     def display(self, show_all_dealer_cards = False):
         print(f'''{"Dealer's" if self.dealer else "Your"} hand:''')
+        
         for index, card in enumerate(self.cards):
             if index == 0 and self.dealer \
             and not show_all_dealer_cards and not self.is_black_jack:
@@ -110,11 +109,12 @@ class Game():
             for i in range(2):
                 player_hand.add_card(deck.deal(1))
                 dealer_hand.add_card(deck.deal(1))
-        
+
             print()
             print("*" * 30)
             print(f"Game {game_number} of {games_to_play}")
             print("*" * 30)
+
             player_hand.display()
             dealer_hand.display()
 
@@ -131,17 +131,17 @@ class Game():
                 if choice in ["HIT","H"]: 
                     player_hand.add_card(deck.deal(1))
                     player_hand.display
-            
+
             if self.check_winner(player_hand, dealer_hand):
                 continue
-            
+
             player_hand_value = player_hand.get_value()
             dealer_hand_value = dealer_hand.get_value()
 
             while dealer_hand_value < 17:
                 dealer_hand.add_card(deck.deal(1))
                 dealer_hand_value = dealer_hand.get_value()
-            
+
             dealer_hand.display(show_all_dealer_cards = True)
 
             if self.check_winner(player_hand, dealer_hand):
@@ -152,7 +152,7 @@ class Game():
             print("Dealers Hand", {dealer_hand_value})
 
             self.check_winner(player_hand,dealer_hand, True)
-            
+
         print("\n Thanks for playing!")
 
     def check_winner(self, player_hand, dealer_hand, game_over=False):
@@ -179,8 +179,8 @@ class Game():
                 print("Player hand smaller than Dealer hand! Dealer Wins!")
             else:
                 print("Player hand equal to Dealer hand! It's a tie!")
-            return True              
+            return True
         return False
 
-g = Game()
-g.play
+game = Game()
+game.play()
