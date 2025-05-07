3fac57350937391053b698f13d2bc94967471b50
James Dickinson <jdickin2@jaguarlandrover.com>
2025-05-07 21:05:44 +0100

    Dart Module Two folder set up and file creation for bin lib and test

---
+++ udemy_dart_project/udemy_dart_module_two/udemy_dart_module_two/test/udemy_dart_module_two_test.dart
@@ -0,0 +1,8 @@
+import 'package:udemy_dart_module_two/udemy_dart_module_two.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
