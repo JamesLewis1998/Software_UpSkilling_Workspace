11837ed74af0362becf42f4241e8c51cdd821ba7
James Dickinson <jdickin2@jaguarlandrover.com>
2025-05-07 21:05:44 +0100

    Dart Module Two folder set up and file creation for bin lib and test

---
+++ udemy_dart_project/udemy_dart_module_two/udemy_dart_module_two/bin/udemy_dart_module_two.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_dart_module_two/udemy_dart_module_two.dart' as udemy_dart_module_two;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_dart_module_two.calculate()}!');
+}
