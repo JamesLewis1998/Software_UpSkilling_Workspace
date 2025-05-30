5528cabf407b6e62f0c6497a49658627e0f95a55
James Dickinson <jdickin2@jaguarlandrover.com>
2025-05-30 10:55:13 +0100

    Udemy Module Two: User Input main.dart file set up

--- udemy_dart_project/udemy_dart_module_two/udemy_dart_module_two_user_input/bin/udemy_dart_module_two_user_input.dart
+++ udemy_dart_project/udemy_dart_module_two/udemy_dart_module_two_user_input/bin/udemy_dart_module_two_user_input.dart
@@ -1,5 +1,5 @@
-import 'package:udemy_dart_module_two_user_input/udemy_dart_module_two_user_input.dart' as udemy_dart_module_two_user_input;
+// Udemy Dart Module Two: User Input
 
-void main(List<String> arguments) {
+main(List<String> arguments) {
   print('Hello world: ${udemy_dart_module_two_user_input.calculate()}!');
 }
