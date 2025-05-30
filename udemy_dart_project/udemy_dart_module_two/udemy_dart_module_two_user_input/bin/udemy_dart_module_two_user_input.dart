97cff4372afd53a1ba1bd0620a900145803586a7
James Dickinson <jdickin2@jaguarlandrover.com>
2025-05-30 10:36:19 +0100

    Udemy Dart Module Two: User Input Structure set up

---
+++ udemy_dart_project/udemy_dart_module_two/udemy_dart_module_two_user_input/bin/udemy_dart_module_two_user_input.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_dart_module_two_user_input/udemy_dart_module_two_user_input.dart' as udemy_dart_module_two_user_input;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_dart_module_two_user_input.calculate()}!');
+}
