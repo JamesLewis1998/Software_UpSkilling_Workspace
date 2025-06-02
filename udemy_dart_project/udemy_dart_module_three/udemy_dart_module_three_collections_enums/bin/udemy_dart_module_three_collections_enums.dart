de4ddd39ee456cf5d6af5945471ff4af4a86f521
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 11:12:45 +0100

    Udemy Dart Module Three: Enum Section Complete

--- udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_enums/bin/udemy_dart_module_three_collections_enums.dart
+++ udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_enums/bin/udemy_dart_module_three_collections_enums.dart
@@ -1,5 +1,21 @@
-import 'package:udemy_dart_module_three_collections_enums/udemy_dart_module_three_collections_enums.dart' as udemy_dart_module_three_collections_enums;
+// Module Three Enum Introduction
+
+  // Enum needs to be defined outside of the function
+    // If you try to define within the function you will recieve an error
+  // Enumerators allow you to do switches which is part of flow control - to be covered in future
+
+enum colours {red,blue,orange}
 
 void main(List<String> arguments) {
-  print('Hello world: ${udemy_dart_module_three_collections_enums.calculate()}!');
+
+  print(colours);
+  // This command just prints out the word colours in the terminal
+
+  // Notice how there's no runtime type, therefore use the following command to print the values out:
+  print(colours.values);
+    // Produces the following in the command terminal: [colours.red, colours.blue, colours.orange]
+
+  // To print individual values: 
+  print(colours.blue);
+
 }
