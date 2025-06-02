a1eb583082026c8b756cccf75d0f862aaf3e4715
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 14:22:39 +0100

    Udemy Dart Module Three: Set Section Complete

--- udemy_dart_project/udemy_dart_module_three/udemy_module_three_collections_set/bin/udemy_module_three_collections_set.dart
+++ udemy_dart_project/udemy_dart_module_three/udemy_module_three_collections_set/bin/udemy_module_three_collections_set.dart
@@ -1,6 +1,18 @@
 // Module Three Collections: Set Overview
-
-
+  // Sets are unordered, they do not contain duplicates
+  // They are very similar to a list 
+  // Have brackets with an E to define specifc types
 void main(List<String> arguments) {
-  print('Hello world: ${udemy_module_three_collections_set.calculate()}!');
+  
+  Set<int> numbers = Set();
+  
+  numbers.add(1);
+  numbers.add(2);
+  numbers.add(3);
+  numbers.add(1); //added twice
+  
+  print(numbers);
+
+  // Output in terminal:
+  // {1, 2, 3} -> because 1 is added twice, the second entry is ignored and not added into the set
 }
