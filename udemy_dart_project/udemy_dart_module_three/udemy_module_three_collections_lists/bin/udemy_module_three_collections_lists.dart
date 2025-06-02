5ed0c27eb4726d49fa22871abbbf1134d93da113
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 11:21:10 +0100

    Udemy Module Three Lists Structure Setup

---
+++ udemy_dart_project/udemy_dart_module_three/udemy_module_three_collections_lists/bin/udemy_module_three_collections_lists.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_module_three_collections_lists/udemy_module_three_collections_lists.dart' as udemy_module_three_collections_lists;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_module_three_collections_lists.calculate()}!');
+}
