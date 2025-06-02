6dc93991f9208768c0d552cace793d487cd9b2ba
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 15:11:25 +0100

    UdmeyModuleThree Collection: Queue SectionComplete

--- udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_queue/bin/udemy_dart_module_three_collections_queue.dart
+++ udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_queue/bin/udemy_dart_module_three_collections_queue.dart
@@ -1,5 +1,22 @@
-import 'package:udemy_dart_module_three_collections_queue/udemy_dart_module_three_collections_queue.dart' as udemy_dart_module_three_collections_queue;
+//Module Three Collections: Queue
+  // Queues: think about queues as a line
+  // Not part of the standard library so we have to import it
+  // Queues are ordered and very quick in memory 
+    // they are ordered, no index and add and remove from start and the end 
+      // there's no ordering with these, you have to add or remove from the end, you can't add or remove from the middle 
 
+import 'dart:collection';
 void main(List<String> arguments) {
-  print('Hello world: ${udemy_dart_module_three_collections_queue.calculate()}!');
+
+Queue items =Queue();
+items.add(1);
+items.add(2);
+items.add(3);
+items.add(4);
+items.removeFirst();
+items.removeLast();
+print(items);
+
+// Only thing left is {2,3} because we've removed the first and the last item being 1 and 4
+
 }
