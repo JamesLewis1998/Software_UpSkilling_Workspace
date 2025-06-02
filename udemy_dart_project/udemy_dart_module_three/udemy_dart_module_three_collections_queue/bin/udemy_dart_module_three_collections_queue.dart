dc6a734379f40233bc64fddc95bddb4928a958ce
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 14:31:28 +0100

    Module Three Collections: Queue Folder Set Up

---
+++ udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_queue/bin/udemy_dart_module_three_collections_queue.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_dart_module_three_collections_queue/udemy_dart_module_three_collections_queue.dart' as udemy_dart_module_three_collections_queue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_dart_module_three_collections_queue.calculate()}!');
+}
