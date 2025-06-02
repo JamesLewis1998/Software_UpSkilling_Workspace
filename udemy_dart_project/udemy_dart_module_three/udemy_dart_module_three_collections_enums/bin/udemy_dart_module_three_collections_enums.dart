2a88c1163c4ad879e055e1f66ec9a569633f0513
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-02 10:57:15 +0100

    Udemy Dart Module Three: Directory Structure SetUp

---
+++ udemy_dart_project/udemy_dart_module_three/udemy_dart_module_three_collections_enums/bin/udemy_dart_module_three_collections_enums.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_dart_module_three_collections_enums/udemy_dart_module_three_collections_enums.dart' as udemy_dart_module_three_collections_enums;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_dart_module_three_collections_enums.calculate()}!');
+}
