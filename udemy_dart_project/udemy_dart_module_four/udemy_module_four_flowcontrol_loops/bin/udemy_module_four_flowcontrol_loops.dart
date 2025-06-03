bba9baeea91c7f2d4748890071d1103d2d1b8e76
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-03 14:44:54 +0100

    Module Four Flow Control: Loops Section Set Up

---
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_loops/bin/udemy_module_four_flowcontrol_loops.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_module_four_flowcontrol_loops/udemy_module_four_flowcontrol_loops.dart' as udemy_module_four_flowcontrol_loops;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_module_four_flowcontrol_loops.calculate()}!');
+}
---
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_loops/test/udemy_module_four_flowcontrol_loops_test.dart
@@ -0,0 +1,8 @@
+import 'package:udemy_module_four_flowcontrol_loops/udemy_module_four_flowcontrol_loops.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
