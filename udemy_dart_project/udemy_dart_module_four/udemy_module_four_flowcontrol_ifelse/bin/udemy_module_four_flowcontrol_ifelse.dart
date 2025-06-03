d01e7801aeab3e85fe6cda360fb43397bb3bc0c6
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-03 08:26:17 +0100

    Udemy Module Four Flow Control Folder Set Up

---
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_ifelse/bin/udemy_module_four_flowcontrol_ifelse.dart
@@ -0,0 +1,5 @@
+import 'package:udemy_module_four_flowcontrol_ifelse/udemy_module_four_flowcontrol_ifelse.dart' as udemy_module_four_flowcontrol_ifelse;
+
+void main(List<String> arguments) {
+  print('Hello world: ${udemy_module_four_flowcontrol_ifelse.calculate()}!');
+}
---
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_ifelse/test/udemy_module_four_flowcontrol_ifelse_test.dart
@@ -0,0 +1,8 @@
+import 'package:udemy_module_four_flowcontrol_ifelse/udemy_module_four_flowcontrol_ifelse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
