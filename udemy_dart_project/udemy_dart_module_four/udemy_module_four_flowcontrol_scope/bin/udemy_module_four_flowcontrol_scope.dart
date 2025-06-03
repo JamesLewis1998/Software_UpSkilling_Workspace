e0a89ebcae1a45adcf761c3a3d54a97774057de1
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-03 09:39:01 +0100

    Udemy Module Four Flow Control: Scope SectionSetup

---
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_scope/bin/udemy_module_four_flowcontrol_scope.dart
@@ -0,0 +1,12 @@
+// Module Four Flow Control: Scope
+  // Scope is where a variable lives
+  // Remember variables are things that will change
+    // Hit the brakes using assertions and throwing errors
+    // steer the car using the if statements
+    // But variables have a short lifecycle 
+
+void main(List<String> arguments) {
+
+
+
+}
