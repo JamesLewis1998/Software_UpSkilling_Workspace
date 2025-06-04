168a32ec5f78405c38aec732909a499963ab1caf
James Dickinson <jdickin2@jaguarlandrover.com>
2025-06-04 13:33:31 +0100

    Udemy Module Four Flow Control: Loops Section 💹

--- udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_loops/bin/udemy_module_four_flowcontrol_loops.dart
+++ udemy_dart_project/udemy_dart_module_four/udemy_module_four_flowcontrol_loops/bin/udemy_module_four_flowcontrol_loops.dart
@@ -1,6 +1,9 @@
 // Module Four Flow Control: Loops 
   // Start of section by creating some variables to use with the loop
 
+  // If you want the function to run at least once, use do 
+  // If you want it to evaluate first before doing any running use while
+
 void main(List<String> arguments) {
   int value;
   int init = 1;
@@ -8,15 +11,49 @@ void main(List<String> arguments) {
 
   value = init;
 
+  // Example One: Evaluate the expression at the end
   do {
-    print(value);
+    print('Value in do loop is $value');
     value++;
   } while(value < max);
-  print('Done with the loop');
+  print('Done with the do loop');
 
-  print(value);
   value = init;                   // need to respecify value because loop will increase to value above
 
+  // Example Two: Evaluate the expression at the start 
+  while(value < max) {
+    print('Value in the while loop is $value');
+    value++;
+  } 
+  print('Done with while loop');
+
+  // Infinite Loops:
+  value = init;
+  do{
+    print('Value = $value');
+    value++;
+
+    if(value == 3){
+      print('function will continue');
+      continue;
+    }
+
+    if(value == 5){
+      print('function will stop');
+      break;
+    }
+
+  } while(true);
+
+  print('Programme Finished');
+    // Common coding mistake above - we've accidently created an infinite loop
+    // this is always going to execute because we don't have a value in the brackets of while
+      // all we have is the value true
+      // the do loop will therefore run infinitely
+
+    // Therefore when we run the programme it executes infinitely in the terminal until we forcefully stop it
+    // In order to fix the above, we need to add in some flow control using continue and break
+      // Add in the condition where we continue if the value == 10 but break if value is 15
 }
 
   // Do and while functions used above
